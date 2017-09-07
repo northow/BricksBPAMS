68a8f14e6668053d02e5f406eccd7b54197b400d
Northow <northow@126.com>
2017-09-07 17:25:23 +0800

    增加Org

---
+++ Developing/BricksBPAMSFrame/Controllers/OrgController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BricksBPAMSFrame.Controllers
+{
+    public class OrgController : Controller
+    {
+        //
+        // GET: /Org/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
